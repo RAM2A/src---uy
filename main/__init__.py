81c958a3907dd6d4f682860670029fde4bb069b0
RAM2A <160558024+RAM2A@users.noreply.github.com>
2024-05-17 10:41:55 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = "24665357" #config("API_ID", default=None, cast=int)
+API_HASH = "beb7e4b83ada668fa85f9a9b56338f1d" #config("API_HASH", default=None)
+BOT_TOKEN = "6561626656:AAEofDQ_-uaIk3vJmiY1xuzjPEeczUXVNt4" #config("BOT_TOKEN", default=None)
+SESSION = "BQF4XQ0AK4XzYLE5k1IGJKNxnmUx2Z8Obg4p1dIp2ZMDtqh5APO5S4Al_XIFqEGNDhSex6-SPiV-XVKY-PZtu0jrluwLhwTZlbrLAzEmrO8OErlOeGOYsiCgkYsk9xDrtN9NVXva3Z9YdXqmW3fMpe0GGKPd_DfQsTC9w5A7ksraWIYiDS808eP8sVJTIIFUb4XdSbTfkfXsYjxKUbm8hVwuaxm66Z-B6KykVAmR0kPkGjlLxH3FzKjV6B0v3gkkQhcReNf49ZRvUa8uw4AlS8cE4H574supRAnG3quK9Sjolj3oZykZ18_CQgyzjKxLgh0IyQGfKtiSpKXW1NW6cB4x1eBa6wAAAABmXyPCAA" #config("SESSION", default=None)
+FORCESUB = "1002137417670" #config("FORCESUB", default=None)
+AUTH = "1717511106" #config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
